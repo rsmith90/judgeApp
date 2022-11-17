89c4a83d17e78e3b3f72ece08c6e584e8a7586f6
Robbie Smith <burt2speed@gmail.com>
2022-11-16 22:31:40 -0500

    added

--- JudgeAPP123/src/App.jsx
+++ JudgeAPP123/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import tailwind from './assets/Tailwind_CSS_Logo.svg'
 import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,15 +10,18 @@ function App() {
   return (
     <div className="App">
       <div>
+
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a href="https://tailwindcss.com/docs" target="_blank">
+          <img src={tailwind} className="logo" alt="Tailwind logo" />
+        </a>
       </div>
-      <h1>Vite + React</h1>
-      <h1>Auto refreshes on save file</h1>
+      <h1>Vite + React + Tailwind</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -26,7 +31,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite + React + Tailwind logos to learn more!
       </p>
     </div>
   )

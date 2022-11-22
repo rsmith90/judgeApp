6fad70cf15bc11a6a426e3849e34669cfe74c79f
Robert Smith <89242101+rsmith90@users.noreply.github.com>
2022-11-22 21:05:25 +0000

    created branch1 test

--- JudgeAPP123/src/components/Test.jsx
+++ JudgeAPP123/src/components/Test.jsx
@@ -4,7 +4,7 @@ import React from "react"
 export default function Test() {
     return (
         <h1 className="text-3xl font-bold underline">
-            Hello world!
+            Branch 1
         </h1>
     )
 }

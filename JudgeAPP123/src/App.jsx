c3aa4e605b8ba3c60c63f28f12a581981b428e96
Robert Smith <89242101+rsmith90@users.noreply.github.com>
2022-11-22 18:18:14 +0000

    updated with daisyUI

--- JudgeAPP123/src/App.jsx
+++ JudgeAPP123/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import tailwind from './assets/Tailwind_CSS_Logo.svg'
 import Wheel from './assets/Wheel.png'
 import Example from './components/Example'
+import Daisy from './assets/Daisy.png'
 import Test from './components/Test'
 import './App.css'
 
@@ -13,7 +14,7 @@ function App() {
   return (
     <div className="App">
       <div>
-        
+
         <Test />
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
@@ -24,8 +25,11 @@ function App() {
         <a href="https://tailwindcss.com/docs" target="_blank">
           <img src={tailwind} className="logo tailwind" alt="Tailwind logo" />
         </a>
+        <a href="https://daisyui.com/docs/install/" target="_blank">
+          <img src={Daisy} className="logo Daisy" alt="DaisyUI logo" />
+        </a>
       </div>
-      <h1>Vite + React + Tailwind</h1>
+      <h1>Vite + React + Tailwind + DaisyUI</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}

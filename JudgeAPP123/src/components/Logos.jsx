1b72b3130cff74198802a82ea713cb94fd65a764
Kevin Solon <kevinsoloniv@gmail.com>
2022-11-29 21:23:57 +0000

    minor layout tweaks

--- JudgeAPP123/src/components/Logos.jsx
+++ JudgeAPP123/src/components/Logos.jsx
@@ -2,21 +2,21 @@ import React from 'react'
 
 export default function Logos() {
   return (
-    <div className="grid grid-flow-col grid-cols-4">
+    <div className="max-w-xl mx-auto grid grid-flow-col grid-cols-4">
         <a href="https://vitejs.dev" target="_blank" className="logo-container">
-          <img src="/viteLogo.svg" className="logo vite" alt="Vite logo" />
+          <img src="/viteLogo.svg" className="max-h-fit logo vite" alt="Vite logo" />
           <p>Vite</p>
         </a>
         <a href="https://beta.reactjs.org/" target="_blank" className="logo-container">
-          <img src="/reactLogo.svg" className="logo react" alt="React logo" />
+          <img src="/reactLogo.svg" className="max-h-fit logo react" alt="React logo" />
           <p>React</p>
         </a>
         <a href="https://tailwindcss.com/docs" target="_blank" className="logo-container">
-          <img src="/tailwindLogo.svg" className="logo tailwind" alt="Tailwind logo" />
+          <img src="/tailwindLogo.svg" className="max-h-fit logo tailwind" alt="Tailwind logo" />
           <p>Tailwind</p>
         </a>
         <a href="https://daisyui.com/docs/install/" target="_blank" className="logo-container">
-          <img src="daisyLogo.png" className="logo daisy" alt="DaisyUI logo" />
+          <img src="daisyLogo.png" className="max-h-fit logo daisy" alt="DaisyUI logo" />
           <p>DaisyUI</p>
         </a>
       </div>

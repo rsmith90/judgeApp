6dd8d35626ee134b04c9c3a72e74d28899d2614d
Kevin Solon <kevinsoloniv@gmail.com>
2022-11-30 20:53:39 +0000

    First theme initial build

--- JudgeAPP123/tailwind.config.cjs
+++ JudgeAPP123/tailwind.config.cjs
@@ -5,10 +5,54 @@ module.exports = {
     "./src/*.{js,ts,jsx,tsx}",
     "./src/components/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+  daisyui: {
+    themes: [
+      {
+        lightTheme01: {
 
-    extend: {},
+          "primary": "#0369a1",
+          "secondary": "#0f766e",
+          "accent": "#4f46e5",
+          "neutral": "#0c4a6e",
+          "base-100": "#192a35",
+          "info": "#7dd3fc",
+          "success": "#2dd4bf",
+          "warning": "#EFD8BD",
+          "error": "#fca5a5",
+
+          "--rounded-box": "1rem", // border radius rounded-box utility class, used in card and other large boxes
+          "--rounded-btn": "0.25rem", // border radius rounded-btn utility class, used in buttons and similar element
+          "--rounded-badge": "1rem", // border radius rounded-badge utility class, used in badges and similar
+          "--animation-btn": "0.2s", // duration of animation when you click on button
+          "--animation-input": "0.2s", // duration of animation for inputs like checkbox, toggle, radio, etc
+          "--btn-text-case": "uppercase", // set default text transform for buttons
+          "--btn-focus-scale": "0.95", // scale transform of button when you focus on it
+          "--border-btn": "1px", // border width of buttons
+          "--tab-border": "1px", // border width of tabs
+          "--tab-radius": "0.5rem", // border radius of tabs
+        },
+      },
+      {
+        darkTheme01: {
+          primary: "#a991f7",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+
+          "--rounded-box": "1rem", // border radius rounded-box utility class, used in card and other large boxes
+          "--rounded-btn": "0.5rem", // border radius rounded-btn utility class, used in buttons and similar element
+          "--rounded-badge": "1.9rem", // border radius rounded-badge utility class, used in badges and similar
+          "--animation-btn": "0.25s", // duration of animation when you click on button
+          "--animation-input": "0.2s", // duration of animation for inputs like checkbox, toggle, radio, etc
+          "--btn-text-case": "uppercase", // set default text transform for buttons
+          "--btn-focus-scale": "0.95", // scale transform of button when you focus on it
+          "--border-btn": "1px", // border width of buttons
+          "--tab-border": "1px", // border width of tabs
+          "--tab-radius": "0.5rem", // border radius of tabs
+        },
+      },
+    ],
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
 }

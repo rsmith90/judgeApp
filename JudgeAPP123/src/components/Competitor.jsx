4fe075ebf591a4c681fc4d97be55a315ba86978a
Kevin Solon <kevinsoloniv@gmail.com>
2022-12-01 18:24:46 +0000

    Light Theme and simplifying Competitor component

--- JudgeAPP123/src/components/Competitor.jsx
+++ JudgeAPP123/src/components/Competitor.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Competitor(person) {
   return (
-    <div className="my-2 px-2 flex bg-accent border-accent-focus border-y-8 border-double rounded-3xl">
+    <div className="m-2 p-2 flex bg-secondary rounded-3xl">
       <img className="h-16 w-16 mask mask-squircle" src={person.image} alt="" />
       <div className="ml-3 divide-y divide-accent-content text-left grow">
           <p className="prose prose-xl font-bold text-accent-content">{person.name}</p>
--- JudgeAPP123/tailwind.config.cjs
+++ JudgeAPP123/tailwind.config.cjs
@@ -8,7 +8,7 @@ module.exports = {
   daisyui: {
     themes: [
       {
-        lightTheme01: {
+        light: {
 
           "primary": "#0369a1",
           "secondary": "#0f766e",
@@ -27,18 +27,22 @@ module.exports = {
           "--animation-input": "0.2s", // duration of animation for inputs like checkbox, toggle, radio, etc
           "--btn-text-case": "uppercase", // set default text transform for buttons
           "--btn-focus-scale": "0.95", // scale transform of button when you focus on it
-          "--border-btn": "1px", // border width of buttons
-          "--tab-border": "1px", // border width of tabs
+          "--border-btn": "2px", // border width of buttons
+          "--tab-border": "2px", // border width of tabs
           "--tab-radius": "0.5rem", // border radius of tabs
         },
       },
       {
-        darkTheme01: {
-          primary: "#a991f7",
-          secondary: "#f6d860",
-          accent: "#37cdbe",
-          neutral: "#3d4451",
-          "base-100": "#ffffff",
+        dark: {
+          "primary": "#93c5fd",
+          "secondary": "#a5b4fc",
+          "accent": "#c4b5fd",
+          "neutral": "#f5f5f4",
+          "base-100": "#e0e7ff",
+          "info": "#bfdbfe",
+          "success": "#bbf7d0",
+          "warning": "#fef9c3",
+          "error": "#fecaca",
 
           "--rounded-box": "1rem", // border radius rounded-box utility class, used in card and other large boxes
           "--rounded-btn": "0.5rem", // border radius rounded-btn utility class, used in buttons and similar element
@@ -47,8 +51,8 @@ module.exports = {
           "--animation-input": "0.2s", // duration of animation for inputs like checkbox, toggle, radio, etc
           "--btn-text-case": "uppercase", // set default text transform for buttons
           "--btn-focus-scale": "0.95", // scale transform of button when you focus on it
-          "--border-btn": "1px", // border width of buttons
-          "--tab-border": "1px", // border width of tabs
+          "--border-btn": "3px", // border width of buttons
+          "--tab-border": "3px", // border width of tabs
           "--tab-radius": "0.5rem", // border radius of tabs
         },
       },
